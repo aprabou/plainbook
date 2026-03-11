5567d63822a619872722f2a2d757f4c0d5738140
Luca de Alfaro <luca@dealfaro.com>
2026-03-11 14:42:13 -0700

    Fixed a path

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,7 +1,7 @@
 # .devcontainer/start.sh
 #!/bin/bash
 export CLAUDE_API_KEY=$(curl -s https://storage.googleapis.com/research-share/claude_public_key.txt)
-cp examples/Soccer.plnb /tmp/Soccer.plnb
+cp /workspaces/plainbook/examples/Soccer.plnb /tmp/Soccer.plnb
 nohup plainbook /tmp/Soccer.plnb > /tmp/plainbook.log 2>&1 &
 sleep 10
 grep 'Authentication token' /tmp/plainbook.log
